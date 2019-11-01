2caf917e8d726ec225b2dc9277f4ab947845296d
Josef Fürlinger <44121556+jfuerlinger@users.noreply.github.com>
2019-11-01 09:18:31 +0100

    add configuration script

---
+++ scripts/configure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+wget https://nodejs.org/dist/latest-v11.x/node-v11.15.0-linux-armv6l.tar.xz
+tar -xf node-v11.15.0-linux-armv6l.tar.xz
+sudo cp -R node-v11.15.0-linux-armv6l/* /usr/local/
+rm node-v11.15.0-linux-armv6l.tar.xz
+rm -R node-v11.15.0-linux-armv6l
+node -v
+
+sudo apt-get install git -y
+
+sudo apt-get install libasound2-dev -y
+
+git clone --depth 1 http://git.videolan.org/git/x264
+cd x264
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+make -j4
+sudo make install
+cd ..
+rm -rf x264
+sudo apt-get install libmp3lame-dev -y
+git clone git://source.ffmpeg.org/ffmpeg --depth=1
+cd ffmpeg
+./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-libmp3lame --enable-nonfree
+make -j4
+sudo make install
+cd ..
+rm -rf ffmpeg
+
+git clone https://github.com/jfuerlinger/leabox
+
+cd leabox
+
+npm install
