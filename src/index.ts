09682bd5b41da77ea7cb07c6d9bd200c5b059e99
Josef Fürlinger <josef.fuerlinger@gmail.com>
2019-11-03 09:06:09 +0000

    implement command line arguments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+const commandLineArgs = require('command-line-args');
 import { LeaBoxController } from './core/leabox-controller';
 import { MongoDbActionDao } from './core/persistence/mongo-db-action-dao';
 import { ActionDao } from './core/persistence/action-dao';
@@ -18,9 +18,6 @@ appInsights.setup()
 // let client = appInsights.defaultClient;
 logger.info('Application Insights configured');
 
-// const leaBoxController = new LeaBoxController();
-// leaBoxController.init();
-// leaBoxController.start();
 
 // const dao: ActionDao = new MongoDbActionDao();
 // dao
@@ -30,33 +27,57 @@ logger.info('Application Insights configured');
 //     })
 //     .catch((err) => console.log(err));
 
-debugger;
 const EventGridClient = require('azure-eventgrid');
 const msRestAzure = require('ms-rest-azure');
 const uuid = require('uuid').v4;
 
-let topicCreds = new msRestAzure.TopicCredentials('anWAk0fYFwgaDG/tL0kHlDWQHZhzXs3P2fn12e2mFdc=');
-let EGClient = new EventGridClient(topicCreds, /*'01964263-bc84-4b67-b94a-2a65733186ef'*/);
-let topicHostName = 'leabox.westeurope-1.eventgrid.azure.net';
-let events = [
-    {
-        id: uuid(),
-        subject: 'TestSubject',
-        dataVersion: '1.0',
-        eventTime: new Date(),
-        eventType: 'JFuerlinger.leabox.testevent',
-        data: {
-            field1: 'value1',
-            filed2: 'value2'
-        }
-    }
+
+
+const optionDefinitions = [
+    { name: 'rfidcontroller', alias: 'r', type: Boolean },
+    { name: 'player', alias: 'p', type: Boolean },
 ];
 
-EGClient.publishEvents(topicHostName, events)
-    .then(
-        (result) => {
-            return Promise.resolve(console.log('Published events successfully.'));
-        }).catch((err) => {
-            console.log('An error ocurred');
-            console.dir(err, { depth: null, colors: true });
-        });
+
+const options = commandLineArgs(optionDefinitions);
+
+if (options.rfidcontroller === true) {
+
+    let topicCreds = new msRestAzure.TopicCredentials('anWAk0fYFwgaDG/tL0kHlDWQHZhzXs3P2fn12e2mFdc=');
+    let EGClient = new EventGridClient(topicCreds);
+    let topicHostName = 'leabox.westeurope-1.eventgrid.azure.net';
+    let events = [
+        {
+            id: uuid(),
+            subject: 'TestSubject',
+            dataVersion: '1.0',
+            eventTime: new Date(),
+            eventType: 'JFuerlinger.leabox.testevent',
+            data: {
+                field1: 'value1',
+                filed2: 'value2'
+            }
+        }
+    ];
+
+    EGClient.publishEvents(topicHostName, events)
+        .then(
+            (result) => {
+                return Promise.resolve(console.log('Published events successfully.'));
+            }).catch((err) => {
+                console.log('An error ocurred');
+                console.dir(err, { depth: null, colors: true });
+            });
+
+} else if (options.player === true) {
+    const leaBoxController = new LeaBoxController();
+    leaBoxController.init();
+    leaBoxController.start();
+} else {
+    const message = 'App was started without mode info!';
+    logger.error(message);
+    new Error(message);
+}
+
+
+
